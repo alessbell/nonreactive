37ec57f6590f2ca61e2c015fb52b313b9eba99b8
alessia <alessia@apollographql.com>
2023-05-31 12:16:14 -0400

    update sandbox

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import {
   ApolloClient,
@@ -6,11 +6,11 @@ import {
   InMemoryCache,
   gql,
   useQuery,
+  useFragment,
+  useMutation,
 } from "@apollo/client";
 import { createFragmentRegistry } from "@apollo/client/cache";
-import Table from "./components/table.jsx";
 import { link } from "./link.js";
-import "./index.css";
 
 export const UserFragment = gql`
   fragment UserFragment on User {
@@ -22,18 +22,177 @@ export const ALL_USERS = gql`
   query AllUsers {
     users {
       id
+      ...UserFragment
+    }
+  }
+`;
+
+export const ALL_USERS_NONREACTIVE = gql`
+  query AllUsers {
+    users {
+      id
       ...UserFragment @nonreactive
     }
   }
 `;
 
-function App() {
-  const { data } = useQuery(ALL_USERS);
+const EDIT_USER = gql`
+  mutation EditUser($name: String, $id: ID) {
+    editUser(name: $name, id: $id) {
+      id
+      name
+    }
+  }
+`;
 
+const ADD_USER = gql`
+  mutation AddUser($name: String) {
+    addUser(name: $name) {
+      id
+      name
+    }
+  }
+`;
+
+function AddUser() {
+  const [name, setName] = React.useState("");
+  const [addUser] = useMutation(ADD_USER, {
+    update: (cache, { data: { addUser: addUserData } }) => {
+      const usersResult = cache.readQuery({ query: ALL_USERS });
+      cache.writeQuery({
+        query: ALL_USERS,
+        data: {
+          ...usersResult,
+          users: [...usersResult.users, addUserData],
+        },
+      });
+    },
+  });
+  return (
+    <div>
+      <div>
+        <label htmlFor="name">Name</label>
+        <div>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Sarah Smith"
+            onChange={(evt) => setName(evt.target.value)}
+            value={name}
+          />
+        </div>
+      </div>
+      <button
+        type="submit"
+        onClick={() => {
+          addUser({ variables: { name } });
+          setName("");
+        }}
+      >
+        Add user
+      </button>
+    </div>
+  );
+}
+
+const UserComponent = function User({ id }) {
+  const { data: user } = useFragment({
+    fragment: UserFragment,
+    from: {
+      __typename: "User",
+      id,
+    },
+  });
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [name, setName] = React.useState(user.name);
+  const [editUser] = useMutation(EDIT_USER);
+  return (
+    <tr>
+      <td>
+        {isEditing ? (
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(e) => {
+              setName(e.currentTarget.value);
+              editUser({
+                variables: { name: e.currentTarget.value, id },
+              });
+            }}
+            value={name}
+          />
+        ) : (
+          <>
+            {name} {Math.random()}
+          </>
+        )}
+      </td>
+      <td>
+        <button
+          onClick={() => {
+            setIsEditing((prevVal) => !prevVal);
+          }}
+        >
+          {isEditing ? "Done editing" : "Edit"} {name}
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+function UsersTable({ query }) {
+  const { data } = useQuery(query);
   return (
     <main>
-      <Table users={data?.users} />
+      <h1>Users</h1>
+      <AddUser />
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.users.map((user) => (
+            <UserComponent key={user.id} id={user.id} />
+          ))}
+        </tbody>
+      </table>
     </main>
+  );
+}
+
+function App() {
+  const [query, setQuery] = React.useState(ALL_USERS);
+  return (
+    <>
+      <div>
+        <label>
+          <input
+            value="slow"
+            type="radio"
+            name="queryVersion"
+            checked={query === ALL_USERS}
+            onChange={() => setQuery(ALL_USERS)}
+          />{" "}
+          Slow
+        </label>
+        <label>
+          <input
+            value="fast"
+            type="radio"
+            name="queryVersion"
+            checked={query === ALL_USERS_NONREACTIVE}
+            onChange={() => setQuery(ALL_USERS_NONREACTIVE)}
+          />{" "}
+          Fast
+        </label>
+      </div>
+      <UsersTable query={query} />
+    </>
   );
 }
 
